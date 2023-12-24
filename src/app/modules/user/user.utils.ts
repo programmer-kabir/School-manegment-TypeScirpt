2b0e8401a54f9061372e0c6d89ee779a129a006a
programmer-kabir <programmerkabirr@gmail.com>
2023-12-24 21:55:07 +0600

    bug fixed, semester and year code

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -5,12 +5,22 @@ const findLastStudent = async () => {
   const lastStudent = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
   .sort({createdAt:-1})
   .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  // console.log(await findLastStudent());
-  const currentId = (await findLastStudent()) || (0).toString();
+  let currentId =(0).toString();
+  const lastStudentId = await findLastStudent();
+  // 2030 01 0001 demo
+  const lastStudentSemesterCode = lastStudentId?.substring(4,6) //01
+  const lastStudentYear  = lastStudentId?.substring(0,4) //2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+ 
+  if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear){
+    currentId = lastStudentId.substring(6)
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
